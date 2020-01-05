40282ee3b9087586d2aa41c1697f847b4b1126ac
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-01-05 14:38:13 -0300

    Configurando acessibilidade e propTypes da página SignIn e SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image } from 'react-native';
+
+import PropTypes from 'prop-types';
 
 import logo from '~/assets/logo.png';
 
@@ -14,6 +16,9 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -25,14 +30,19 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
 
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
-          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText>Criar conta gratuita</SignLinkText>
@@ -41,3 +51,9 @@ export default function SignIn({ navigation }) {
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
